5e9f5a9c5c2dea7fb811a7819f2e9f82b2198696
Joao Cordeiro <jschmidtcordeiro@gmail.com>
2024-10-13 22:20:58 -0300

    feat: adicionar calculo de firstpos, lastpos e nullable

--- tree-test.py
+++ tree-test.py
@@ -11,6 +11,10 @@ class Node:
 
     def __repr__(self, level=0, prefix="Root: "):
         result = "\t" * level + prefix + repr(self.value) + f" (ID: {self.id})\n"
+        result += f"Nullable: {self.nullable}\n"
+        result += f"Firstpos: {self.firstpos}\n"
+        result += f"Lastpos: {self.lastpos}\n"
+        result += f"Followpos: {self.followpos}\n"
         if self.left:
             result += self.left.__repr__(level + 1, "L--- ")
         if self.right:
@@ -32,30 +36,46 @@ def parse_regex(expr):
             elif expr[i] == '*':
                 # Apply star operation to the last node
                 last_node = nodes.pop()
-                node_id += 1
-                nodes.append(Node('*', left=last_node, node_id=node_id))
+                node = Node('*', left=last_node)
+                node.nullable = True
+                node.firstpos = last_node.firstpos
+                node.lastpos = last_node.lastpos
+                nodes.append(node)
             elif expr[i] == '|':
                 # Union operation (needs two children)
-                left = Node('|', left=nodes.pop(), node_id=node_id)
+                left = Node('|', left=nodes.pop())
                 subexpr, i = parse_expression(i + 1, node_id)
                 left.right = subexpr
+                left.nullable = left.left.nullable or left.right.nullable
+                left.firstpos = left.left.firstpos.union(left.right.firstpos)
+                left.lastpos = left.left.lastpos.union(left.right.lastpos)
                 nodes.append(left)
+            elif expr[i] == '&':
+                # Append symbol (concatenation handled later)
+                node = Node(expr[i])
+                node.nullable = True
+                node.firstpos = set()
+                node.lastpos = set()
+                nodes.append(node)
             else:
                 # Append symbol (concatenation handled later)
                 node_id += 1
-                nodes.append(Node(expr[i], node_id=node_id))
+                node = Node(expr[i], node_id=node_id)
+                node.nullable = False
+                node.firstpos = {node_id}
+                node.lastpos = {node_id}
+                nodes.append(node)
             i += 1
         
         # Now handle concatenation
         while len(nodes) > 1:
             left = nodes.pop(0)
             right = nodes.pop(0)
-            node_id += 1
-            concat_node = Node('.', left, right, node_id=node_id)
+            concat_node = Node('.', left, right)
             # Update nullable, firstpos, lastpos for concatenation
             concat_node.nullable = left.nullable and right.nullable
-            concat_node.firstpos = left.firstpos if left.nullable else left.firstpos.union(right.firstpos)
-            concat_node.lastpos = right.lastpos if right.nullable else left.lastpos.union(right.lastpos)
+            concat_node.firstpos = left.firstpos.union(right.firstpos) if left.nullable else left.firstpos
+            concat_node.lastpos = right.lastpos.union(left.lastpos) if right.nullable else right.lastpos
             nodes.insert(0, concat_node)
         
         return nodes[0], i
@@ -66,6 +86,7 @@ def parse_regex(expr):
 # Test the code with a regular expression
 regex = "aa*((b|b*)aa*b)*"
 regex = "aa*"
+regex = "&|a|bb*"
 tree = parse_regex(regex)
 print(tree)
 
